f3dca60ca1a2f7e0647e79fabb2773dea79a108a
ArpitVishwakarma777 <wwArpit@gmail.com>
2025-01-13 21:24:19 +0530

    connect with atlas

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const express = require("express");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const dbUrl = "mongodb://127.0.0.1:27017/tiffin-tadaka";
-connection(dbUrl)
+// const dbUrl = "mongodb+srv://ArpitVishwakarma:root12345@cluster0.jmm11.mongodb.net/tiffin-tadaka";
+connection(process.env.DB_URL)
   .then(() => {
     console.log("Connection is stablisted with DB ");
   })
@@ -22,6 +22,6 @@ app.use("/api/home", router);
 app.use("/api/menu", menuDataRouter);
 app.use("/api", signUpRouter);
 app.use("/api/contact", contactRouter);
-app.listen("8000", () => {
+app.listen(`${process.env.BACKEND_PORT}`, () => {
   console.log("server is running on port 8000");
 });
